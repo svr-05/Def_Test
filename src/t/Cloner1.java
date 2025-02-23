680fcb5b616559d35b34f325a86296cf429ca370
evargasm19 <evargasm19@gmail.com>
2025-02-22 23:23:55 -0500

    changes made by cloner1

---
+++ src/t/Cloner1.java
@@ -0,0 +1,8 @@
+package t;
+
+public class Cloner1 {
+	public static void main(String[] args) {
+		System.out.println("Edwin is cloner1");
+	}
+
+}
--- src/t/Test.java
+++ src/t/Test.java
@@ -2,7 +2,7 @@ package t;
 
 public class Test {
 	public static void main(String[] args) {
-		System.out.println("T");
+		System.out.println("T_ esta modificacion por edwin");
 	}
 
 }
